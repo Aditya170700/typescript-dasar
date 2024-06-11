b828b8a58881fe8d2db281ec3258d043a550e321
Aditya170700 <adityaric72@gmail.com>
2024-06-12 06:02:16 +0700

    Primitif data type

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data TypeScript', () => {
+    it('should must declare', () => {
+        const name: string = "Aditya Ricki";
+        const balance: number = 1000000;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
