66fec2240a52d3c261b29347c9378c1c869767cd
Aditya170700 <adityaric72@gmail.com>
2024-06-12 19:19:30 +0700

    Default value

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -1,9 +1,18 @@
 describe('function', () => {
-    it('returns true when no arguments', () => {
+    it('should support function', () => {
         function sayHello(name: string): string {
             return `Hello ${name}`;
         }
 
         expect(sayHello('Aditya')).toBe('Hello Aditya');
-    })
-})
+    });
+
+    it('should support function default value', () => {
+        function sayHello(name: string = 'Guest'): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello('Aditya')).toBe('Hello Aditya');
+        expect(sayHello()).toBe('Hello Guest');
+    });
+});
