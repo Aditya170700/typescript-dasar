2fc8f6c7d0705d213002930db801fbe94d23fe08
Aditya170700 <adityaric72@gmail.com>
2024-06-12 18:14:14 +0700

    Interface function

--- tests/seller.test.ts
+++ tests/seller.test.ts
@@ -1,7 +1,7 @@
 import {Seller} from "../src/seller";
 
 describe('seller', () => {
-    it('Should be able to sell a seller', async () => {
+    it('Should be able to sell a seller', () => {
         const seller: Seller = {
             id: 1,
             name: 'John Doe',
@@ -12,4 +12,16 @@ describe('seller', () => {
 
         console.info(seller);
     });
+
+    it('Should support function iface', () => {
+        interface AddFunction {
+            (value1: number, value2: number): number;
+        }
+
+        const add: AddFunction = (value1, value2) => {
+            return value1 + value2;
+        }
+
+        console.log(add(1, 2));
+    })
 });
