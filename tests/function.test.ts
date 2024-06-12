6e9f10729527d797d649bf9176e34d583598f11c
Aditya170700 <adityaric72@gmail.com>
2024-06-12 19:19:43 +0700

    Rest param function

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -15,4 +15,18 @@ describe('function', () => {
         expect(sayHello('Aditya')).toBe('Hello Aditya');
         expect(sayHello()).toBe('Hello Guest');
     });
+
+    it('should rest params', () => {
+        function sum(...values: number[]): number {
+            let total = 0;
+
+            for (const value of values) {
+                total += value;
+            }
+
+            return total;
+        }
+
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
+    });
 });
